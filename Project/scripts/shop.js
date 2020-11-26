2dfd8a9ab742123ef5c1667c55452981eb51ecc2
Pavel Romanov <pavel.romanov101@gmail.com>
2020-11-27 00:38:22 +0200

    shop

---
+++ Project/scripts/shop.js
@@ -0,0 +1,43 @@
+function hover1(element) {
+    element.setAttribute('src', 'photos/jacket1-1...jpg');
+  }
+  
+  function unhover1(element) {
+    element.setAttribute('src', 'photos/jacket1.jpg');
+  }
+
+  function hover2(element) {
+    element.setAttribute('src', 'photos/jacket2-2.jpg');
+  }
+  
+  function unhover2(element) {
+    element.setAttribute('src', 'photos/jacket2.jpg');
+  }
+  function hover3(element) {
+    element.setAttribute('src', 'photos/jacket3-3.jpg');
+  }
+  
+  function unhover3(element) {
+    element.setAttribute('src', 'photos/jacket3.jpg');
+  }
+  function hover4(element) {
+    element.setAttribute('src', 'photos/jacket4-4.jpg');
+  }
+  
+  function unhover4(element) {
+    element.setAttribute('src', 'photos/jacket4.jpg');
+  }
+  function hover5(element) {
+    element.setAttribute('src', 'photos/jacket5-5.jpg');
+  }
+  
+  function unhover5(element) {
+    element.setAttribute('src', 'photos/jacket5.jpg');
+  }
+  function hover6(element) {
+    element.setAttribute('src', 'photos/jacket6-6.jpg');
+  }
+  
+  function unhover6(element) {
+    element.setAttribute('src', 'photos/jacket6.jpg');
+  }
